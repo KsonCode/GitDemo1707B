eb84d7ac1787eb2a83d2f4acb085b349965dd4c5
KsonCode <19655910@qq.com>
2019-11-29 15:50:18 +0800

    主分支代码

--- app/src/main/java/com/bwie/gitdemo/MainActivity.java
+++ app/src/main/java/com/bwie/gitdemo/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //主分支代码
+        System.out.println("主分支log");
 
 
     }
