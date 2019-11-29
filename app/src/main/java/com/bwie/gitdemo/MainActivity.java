57b4b29f36064e3c9fd595cb91732a7fdf2b06d4
KsonCode <19655910@qq.com>
2019-11-29 15:51:36 +0800

    dev

--- app/src/main/java/com/bwie/gitdemo/MainActivity.java
+++ app/src/main/java/com/bwie/gitdemo/MainActivity.java
@@ -14,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //dev分支代码
+        System.out.println("dev");
 
     }
 }
